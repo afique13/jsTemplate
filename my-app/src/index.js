8f99d88ddd0ff53bc22cbb88716fc3fea6efd566
Afiq Azizul <afique13@gmail.com>
2021-02-23 02:48:46 +0800

    react cli cant work for some reason

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,22 @@
-/** @jsx React.DOM */
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector("#link-1").onclick = changeClass;
+});
+
+function changeClass (){
+    const flag = document.querySelector("#react-icon").classList.item(1);
+    if(flag === 'fa-bars'){
+        document.querySelector("#react-icon").classList.replace("fa-bars", "fa-times");
+    }
+    else if(flag === 'fa-times'){
+        console.log('5');
+        document.querySelector("#react-icon").classList.replace("fa-times", "fa-bars");
+    }
+}   
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
